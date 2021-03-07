57c09f098cae979c1b98bfb3baae2970fc3d5e50
Isaac <isaacgeazy@gmail.com>
2021-03-07 12:50:57 -0300

    Atividade GRITADOR

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _05_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string diga;
+            
+        
+            diga = Convert.ToString(Console.ReadLine());
+            Console.WriteLine($"{diga.ToUpper()}");
+
+        }
+    }
+}
